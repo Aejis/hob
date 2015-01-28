96bd4a34623ac0bc787b1f7f578d1f2ab7bb253d
Andrey Savchenko <andrey@aejis.eu>
2015-01-28 18:03:24 +0200

    Change quotes

--- lib/hob/migrator.rb
+++ lib/hob/migrator.rb
@@ -5,13 +5,13 @@ module Hob
   class Migrator
     def self.migrate(version = nil)
       Sequel.extension :migration
-      
+
       if version
         puts "Migrating to version #{version}"
-        Sequel::Migrator.run(World.db, "./migrations", target: version)
+        Sequel::Migrator.run(World.db, './migrations', target: version)
       else
-        puts "Migrating to latest"
-        Sequel::Migrator.run(World.db, "./migrations")
+        puts 'Migrating to latest'
+        Sequel::Migrator.run(World.db, './migrations')
       end
     end
   end
