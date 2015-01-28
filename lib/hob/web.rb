ea2c6b51a8806c4b0fb5cf54125dac38b657bd64
Andrey Savchenko <andrey@aejis.eu>
2015-01-28 16:31:40 +0200

    Initial API

--- lib/hob/web.rb
+++ lib/hob/web.rb
@@ -1,5 +1,51 @@
 module Hob
   class Web < Sinatra::Base
+    get '/' do
+
+    end
+
+    # Create
+    #
+    # Params:
+    # - name
+    # - repo
+    # - branch
+    # - ruby_version
+    # - prepare_commands
+    # - run_commands
+    #
+    get '/apps/create' do
+
+    end
+
+    post '/apps/.?:format?' do
+
+    end
+
+    # Show app
+    get '/apps/:name.?:format?' do
+
+    end
+
+    # Restart app
+    get '/apps/:name/restart.?:format?' do
+
+    end
+
+    # Stop app
+    get '/apps/:name/stop.?:format?' do
+
+    end
+
+    # Start app
+    get '/apps/:name/start.?:format?' do
+
+    end
+
+    # Show build
+    get '/apps/:name/build/:id.?:format?' do
+
+    end
 
   end
 end
