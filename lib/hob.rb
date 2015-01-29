ddc7a5ec95ff6a020d2b163d0536a9e4a7e532b5
Denis Sergienko <olol.toor@gmail.com>
2015-01-29 15:04:56 +0200

    env run

--- lib/hob.rb
+++ lib/hob.rb
@@ -6,13 +6,7 @@ require 'sqlite3'
 module Hob
 end
 
-<<<<<<< HEAD
 require 'hob/web'
 require 'hob/world'
 require 'hob/application/paths'
 require 'hob/application'
-=======
-require_relative 'hob/web'
-require_relative 'hob/world'
-require_relative 'hob/tasks'
->>>>>>> denis
--- lib/hob/command.rb
+++ lib/hob/command.rb
@@ -8,14 +8,16 @@ module Hob
 
     attr_reader :fail_log
 
-    def initialize(command, dir)
+    def initialize(command, dir, params)
       @command = command
 
       @options = {
         chdir: dir
       }
 
-      run_command
+      run_in_env(params[:rubies_path], params[:version], params[:exports]) do
+        run_command
+      end
     end
 
     def run_command
@@ -28,6 +30,24 @@ module Hob
       rescue Errno::ENOENT => e
         @fail_log = e.message
       end
+    end
+
+    private
+
+    def run_in_env(rubies_path, version, exports)
+      exports ||= {}
+      old_path = ENV["PATH"]
+
+      ruby_root = File.join(rubies_path, "ruby-#{version}/bin") if version
+      ruby_root ||= `which ruby`
+
+      ENV["PATH"] = ruby_root + ':' + ENV["PATH"]
+      exports.each { |k, v| ENV[k] = v }
+
+      yield
+
+      ENV["PATH"] = old_path
+      exports.each { |k, _| ENV[k] = nil }
     end
   end
 end
